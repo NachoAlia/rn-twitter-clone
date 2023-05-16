6102b70e43f143ec72f8fcb0f0690d636d9c1c9e
duemarfra <duemarfra@gmail.com>
2023-05-16 11:12:34 -0300

    test algo

---
+++ src/components/Algo.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Algo = () => {
+  return (
+    <View>
+      <Text>Algo</Text>
+    </View>
+  );
+};
+
+export default Algo;
+
+const styles = StyleSheet.create({});

8ed14fed708da3b39e16c99ebfc53ff710937ede
Nacho Alia <nachoalianiello@gmail.com>
2023-06-02 14:21:25 -0300

    Added: Changes in info user

---
+++ src/components/Account/index.js
@@ -0,0 +1,6 @@
+export * from "./InfoUser";
+export * from "./UserButtonGroup";
+export * from "./UserLikes";
+export * from "./UserMedia";
+export * from "./UserPosts";
+export * from "./UserReplies";
--- src/components/DrawerContent/DrawerOptions/DrawerOptions.js
+++ src/components/DrawerContent/DrawerOptions/DrawerOptions.js
@@ -6,14 +6,72 @@ import { useNavigation } from "@react-navigation/native";
 import { screen } from "../../../utils/screenName";
 export function DrawerOptions() {
   const navigation = useNavigation();
-  const menuOptions = getMenuOptions(navigation);
+
+  const getMenuOptions = () => {
+    return [
+      {
+        title: "Profile",
+        iconType: "material-community",
+        iconNameLeft: "account-outline",
+        iconSize: 32,
+        iconColorLeft: "#ccc",
+        bottomDivider: false,
+        onPress: () => navigation.navigate(screen.account.account),
+      },
+      {
+        title: "Explore",
+        iconType: "material-community",
+        iconNameLeft: "magnify",
+        iconSize: 32,
+        bottomDivider: false,
+        onPress: () => console.log("holi1"),
+      },
+      {
+        title: "List",
+        iconType: "material-community",
+        iconNameLeft: "clipboard-outline",
+        iconSize: 32,
+        bottomDivider: false,
+        onPress: () => console.log("asd"),
+      },
+      {
+        title: "Bookmark",
+        iconType: "material-community",
+        iconNameLeft: "bookmark-outline",
+        iconSize: 32,
+        bottomDivider: true,
+        onPress: () => console.log("asd"),
+      },
+      {
+        title: "Settings",
+        iconType: "material-community",
+        iconNameLeft: "cog-outline",
+        iconSize: 32,
+        bottomDivider: false,
+        onPress: () => console.log("asd"),
+      },
+      {
+        title: "Help Center",
+        iconType: "material-community",
+        iconNameLeft: "chat-question-outline",
+        iconSize: 32,
+        bottomDivider: false,
+        onPress: () => console.log("asd"),
+      },
+    ];
+  };
+
+  const menuOptions = getMenuOptions();
   return (
     <View>
       {menuOptions.map((elem, index) => {
         return (
-          <>
-            {index == 4 && <View style={styles.bottomDivider}></View>}
-            <TouchableOpacity style={styles.touchable} onPress={elem.onPress}>
+          <View key={index}>
+            <TouchableOpacity
+              style={styles.touchable}
+              onPress={elem.onPress}
+              key={index}
+            >
               <Icon
                 type={elem.iconType}
                 name={elem.iconNameLeft}
@@ -22,58 +80,10 @@ export function DrawerOptions() {
               />
               <Text style={{ fontSize: 22 }}>{elem.title}</Text>
             </TouchableOpacity>
-          </>
+            {elem.bottomDivider && <View style={styles.bottomDivider}></View>}
+          </View>
         );
       })}
     </View>
   );
-
-  function getMenuOptions(navigation) {
-    return [
-      {
-        title: "Profile",
-        iconType: "material-community",
-        iconNameLeft: "account-outline",
-        iconSize: 32,
-        iconColorLeft: "#ccc",
-        onPress: () => navigation.navigate(screen.account.account),
-      },
-      {
-        title: "Explore",
-        iconType: "material-community",
-        iconNameLeft: "magnify",
-        iconSize: 32,
-
-        onPress: () => console.log("holi1"),
-      },
-      {
-        title: "List",
-        iconType: "material-community",
-        iconNameLeft: "clipboard-outline",
-        iconSize: 32,
-        onPress: () => console.log("asd"),
-      },
-      {
-        title: "Bookmark",
-        iconType: "material-community",
-        iconNameLeft: "bookmark-outline",
-        iconSize: 32,
-        onPress: () => console.log("asd"),
-      },
-      {
-        title: "Settings",
-        iconType: "material-community",
-        iconNameLeft: "cog-outline",
-        iconSize: 32,
-        onPress: () => console.log("asd"),
-      },
-      {
-        title: "Help Center",
-        iconType: "material-community",
-        iconNameLeft: "chat-question-outline",
-        iconSize: 32,
-        onPress: () => console.log("asd"),
-      },
-    ];
-  }
 }

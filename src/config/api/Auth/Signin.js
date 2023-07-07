ac7c5fa4db33e61017073f9456cfef00a8f9e7ec
duemarfra <duemarfra@gmail.com>
2023-07-07 13:01:00 -0300

    added AsyncStorage for save token in cache

--- src/config/api/Auth/Signin.js
+++ src/config/api/Auth/Signin.js
@@ -1,4 +1,5 @@
 import { domainUrl } from "../../host";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const route = "/users/login";
 
@@ -21,5 +22,35 @@ export async function Signin(emailOrUsername, password) {
     throw new Error(errorResponse.error);
   }
 
-  return await response.json();
+  const data = await response.json();
+
+  await AsyncStorage.setItem("token", data.token);
+
+  return data;
 }
+
+// import { domainUrl } from "../../host";
+
+// const route = "/users/login";
+
+// const apiUrl = `${domainUrl}${route}`;
+
+// export async function Signin(emailOrUsername, password) {
+//   const response = await fetch(apiUrl, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       email_or_username: emailOrUsername,
+//       password: password,
+//     }),
+//   });
+
+//   if (!response.ok) {
+//     const errorResponse = await response.json();
+//     throw new Error(errorResponse.error);
+//   }
+
+//   return await response.json();
+// }
--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -7,9 +7,45 @@ import { AccountDrawer } from "./AccountDrawer";
 import { screen } from "../utils";
 import { AppNavigation } from "./AppNavigation";
 import { AccountStack } from "./AccountStack";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+
 const Stack = createNativeStackNavigator();
 
 export function AppStack() {
+  const navigation = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const checkToken = async () => {
+    // Verificar la existencia de un token almacenado
+    const token = await AsyncStorage.getItem("token");
+
+    if (token) {
+      // Realizar la autenticación automática utilizando el token
+      // Aquí puedes implementar la lógica para verificar si el token es válido
+      // y realizar cualquier acción necesaria, como cargar los datos del usuario
+      // desde la API.
+
+      // Ejemplo básico:
+      // Si el token es válido, redirigir a la pantalla principal
+      // navigation.navigate(screen.account.index);
+
+      console.log(token);
+    }
+
+    // setIsLoading(false);
+  };
+
+  // if (isLoading) {
+  //   // Mostrar una pantalla de carga mientras se verifica el token
+  //   return null;
+  // }
+
   return (
     <Stack.Navigator initialRouteName={screen.account.signin}>
       <Stack.Screen

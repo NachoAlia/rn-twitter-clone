d6f52198488068ead0befd1a18601fdb355ffcd8
duemarfra <duemarfra@gmail.com>
2023-08-28 16:48:58 -0300

    added navigation friends

---
+++ src/screens/Friends/FriendsScreen/FriendsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { styles } from './FriendsScreen.styles'
+
+export function FriendsScreen() {
+  return (
+    <View>
+      <Text>FriendsScreen</Text>
+    </View>
+  )
+}

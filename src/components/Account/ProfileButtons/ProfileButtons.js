b09711547749a55535eb36b95eb52eb8eb0627f5
duemarfra <duemarfra@gmail.com>
2023-08-29 16:50:55 -0300

    edit notifications button

--- src/components/Account/ProfileButtons/ProfileButtons.js
+++ src/components/Account/ProfileButtons/ProfileButtons.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { Icon } from "react-native-elements";
 import { styles } from "./ProfileButtons.styles";
 import { useNavigation } from '@react-navigation/native'
 import { screen } from '../../../utils/screenName'
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { sendFriendRequest, acceptFriendRequest } from "../../../config/api/Friends/Friendships";
 
 export const ProfileButtons = ({
   isCurrentUser,
@@ -14,9 +12,6 @@ export const ProfileButtons = ({
 }) => {
 
   const [isFriend, setIsFriend] = useState(false)
-  const [hasPendingRequest, setHasPendingRequest] = useState(false);
-  const [friendshipId, setFriendshipId] = useState(null);
-
   // const [notifications, setNotifications] = useState(false)
 
   const navigation = useNavigation();
@@ -25,79 +20,13 @@ export const ProfileButtons = ({
     navigation.navigate(screen.account.editProfile);
   };
 
-  const checkFriendshipStatus = async () => {
-    try {
-      const token = await AsyncStorage.getItem("token");
-      const response = await fetch(`${apiUrl}/status/${otherPersonId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (!response.ok) {
-        const errorResponse = await response.json();
-        throw new Error(errorResponse.error);
-      }
-
-      const data = await response.json();
-      setIsFriend(data.isFriend);
-      setHasPendingRequest(data.hasPendingRequest);
-      setFriendshipId(data.friendshipId);
-    } catch (error) {
-      console.error("Error checking friendship status:", error);
+  const handlerFriendship = () => {
+    if (isFriend = false) {
+      // si no es amigo debe enviar una solicitud de amistad
+    } else {
+      // si ya es amigo debe eliminar la amistad
     }
-  };
-
-  const handleFriendship = async () => {
-    try {
-      const token = await AsyncStorage.getItem("token");
-
-      if (isFriend) {
-        // Si ya son amigos, eliminar la amistad
-        const response = await fetch(`${apiUrl}/${friendshipId}`, {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (!response.ok) {
-          const errorResponse = await response.json();
-          throw new Error(errorResponse.error);
-        }
-
-        setIsFriend(false);
-      } else {
-        // Si no son amigos, enviar una solicitud de amistad
-        const response = await sendFriendRequest(otherPersonId, token);
-
-        if (response.message === "Friend request sent.") {
-          setIsFriend(true);
-        }
-      }
-    } catch (error) {
-      console.error("Error handling friendship:", error);
-    }
-  };
-
-  const acceptFriendship = async () => {
-    try {
-      const token = await AsyncStorage.getItem("token");
-
-      const response = await acceptFriendRequest(friendshipId, token);
-
-      if (response.message === "Friend request accepted.") {
-        setIsFriend(true);
-        setHasPendingRequest(false);
-      }
-    } catch (error) {
-      console.error("Error accepting friendship:", error);
-    }
-  };
-
-  useEffect(() => {
-    checkFriendshipStatus();
-  }, []);
+  }
 
   return (
     <View style={styles.buttonsContainer}>
@@ -111,15 +40,15 @@ export const ProfileButtons = ({
       ) : (
 
         <>
-          <TouchableOpacity style={styles.followButton} onPress={() => console.log(` ${myId} jajaj ${otherPersorId} `)}>
-            <Text style={styles.followButtonText}>
+          <TouchableOpacity style={styles.friendButton} onPress={() => console.log(` ${myId} jajaj ${otherPersonId} `)}>
+            <Text style={styles.friendButtonText}>
               {isFriend ? "Friend" : "Add Friend"}
             </Text>
           </TouchableOpacity>
 
           {/* <TouchableOpacity
             style={styles.notificationsButton}
-            onPress={console.log(notifications)}
+            onPress={() => console.log(notifications)}
           >
             <Icon
               name="bell-outline"
@@ -128,6 +57,7 @@ export const ProfileButtons = ({
               size={20}
             />
           </TouchableOpacity> */}
+
         </>
       )}
     </View>

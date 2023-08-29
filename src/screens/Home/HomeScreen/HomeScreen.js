c3d8c173ce7719f12eb69696a9b60a3855b7a4d9
RamEloisa <ram.eloisalg@gmail.com>
2023-08-29 12:17:51 -0300

    Added: more-posts

--- src/screens/Home/HomeScreen/HomeScreen.js
+++ src/screens/Home/HomeScreen/HomeScreen.js
@@ -1,23 +1,39 @@
 import React, { useState } from "react";
-import { View, RefreshControl, FlatList } from "react-native";
+import {
+  View,
+  RefreshControl,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { ButtonNewPost } from "../../../components";
 import { Post } from "../../../components/Posts";
 import { useThemaContext } from "../../../components/ThemeProvider";
 import { color } from "../../../utils";
 
-import { usePostsContext, usereloadPostContext } from "../../../context";
+import {
+  useMorePostsContext,
+  usePostsContext,
+  usereloadPostContext,
+} from "../../../context";
 
 export function HomeScreen() {
   const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const thema = useThemaContext();
   const dataPosts = usePostsContext();
+  const morePosts = useMorePostsContext();
   const reloadpost = usereloadPostContext();
 
   const onRefresh = async () => {
     setRefreshing(true);
     await reloadpost();
     setRefreshing(false);
+  };
+
+  const loadMorePosts = async () => {
+    setLoading(true);
+    await morePosts().then(setLoading(false));
   };
   return (
     <View
@@ -34,6 +50,8 @@ export function HomeScreen() {
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
+          onEndReached={loadMorePosts}
+          ListFooterComponent={() => (loading ? <ActivityIndicator /> : null)}
         />
       )}
       <ButtonNewPost />
